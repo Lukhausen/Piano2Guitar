019b202b01273fa16d98265464f4646fef49c3be
Luke <mail@lukhausen.de>
2024-05-03 23:37:45 +0200

    style problem and settings added

--- main-application/script.js
+++ main-application/script.js
@@ -6,11 +6,20 @@ import CustomPopup from "./pup-up.js"
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    //Make Settings Button functional
+    window.toggleSettings = function () {
+        let settingsButton = document.getElementById("settings");
+        // Toggle a class that controls the visibility and opacity
+        settingsButton.classList.toggle('visible');
+    }
+    
+
+
     // Import the MidiManager
     const midiManager = new MIDIAccessManager();
     const popUp = new CustomPopup()
 
-    window.closePopup = function(){
+    window.closePopup = function () {
         popUp.close()
     }
 
@@ -27,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return visualPianoOctaves;
     }
 
-    
+
 
     // Create a new Piano instance with dynamic number of octaves
     const myPiano = new Piano('.pianoContainer', { octaves: calculateOctaves() });
@@ -50,10 +59,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     var isVolumeOn = localStorage.getItem('volumeState') === 'off' ? false : true;
+    const volumeIcon = document.getElementById('volumeIcon');
+
     updateVolumeIcon(); // Update the icon at load
 
     window.toggleVolume = function () {
-        const volumeIcon = document.querySelector('.visualPianoIcon');
 
         if (isVolumeOn) {
             myPiano.volumeOff(); // Turn volume off
@@ -68,7 +78,6 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     function updateVolumeIcon() {
-        const volumeIcon = document.querySelector('.visualPianoIcon');
         if (!isVolumeOn) {
             volumeIcon.innerHTML = `<path d="M792-56 671-177q-25 16-53 27.5T560-131v-82q14-5 27.5-10t25.5-12L480-368v208L280-360H120v-240h128L56-792l56-56 736 736-56 56Zm-8-232-58-58q17-31 25.5-65t8.5-70q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 53-14.5 102T784-288ZM650-422l-90-90v-130q47 22 73.5 66t26.5 96q0 15-2.5 29.5T650-422ZM480-592 376-696l104-104v208Zm-80 238v-94l-72-72H200v80h114l86 86Zm-36-130Z"/>`;
         } else {
